9b93bcae7013851f777dc0ba9ab8aac28457c03b
Aaditya jha <aadityajha2000@gmail.com>
2020-10-12 15:13:17 +0545

    Express REST API and Mongoose

--- app.js
+++ app.js
@@ -28,6 +28,16 @@ app.use("/dishes", dishRouter);
 app.use("/leaders", leaderRouter);
 app.use("/promotions", promoRouter);
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) =>{
+  console.log("Connected Correctly to Server!");
+}, (err) => console.log(err));
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
