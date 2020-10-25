6d9223de042edcee45212e12a85997bfc3d9e79b
Aaditya jha <aadityajha2000@gmail.com>
2020-10-25 18:19:58 +0545

    Uploading Files

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var usersRouter = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
+const uploadRouter = require('./routes/uploadRouter');
 
 var app = express();
 
@@ -69,6 +70,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/leaders", leaderRouter);
 app.use("/promotions", promoRouter);
+app.use('/imageUpload',uploadRouter);
 
 const mongoose = require('mongoose');
 
