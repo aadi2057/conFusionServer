145fb464b291428d6e855b36691df467fe3a5456
Aaditya jha <aadityajha2000@gmail.com>
2020-10-27 15:28:33 +0545

    Passport Facebook using OAuth 2

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '706219816663397',
+        clientSecret: '1a705550eeb10ea49ec403fdf42abe8e'
+    }
 }
